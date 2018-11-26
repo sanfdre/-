fa89a3da690ccc475ec172ecf22edd2d3e2edca2
sanfdre <1160191049@qq.com>
2018-11-26 21:33:58 +0800

    share friend

--- mapreduce/src/main/java/ShareFriend/ShareFriendStepTwo.java
+++ mapreduce/src/main/java/ShareFriend/ShareFriendStepTwo.java
@@ -1,10 +1,1 @@
-package ShareFriend;
-
-import org.apache.hadoop.mapreduce.Mapper;
-
-
-public class ShareFriendStepTwo {
-//    static class ShareFriendStepTwoMapper extends Mapper<>{
-//
-//    }
-}
+package ShareFriend;import org.apache.hadoop.mapreduce.Mapper;public class ShareFriendStepTwo {//    static class ShareFriendStepTwoMapper extends Mapper<>{////    }}
