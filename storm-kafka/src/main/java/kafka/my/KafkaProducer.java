574dd94680459f541b0cab9407fa2f082eac0852
sanfdre <1160191049@qq.com>
2019-01-30 09:46:50 +0800

    kafka example

--- storm-kafka/src/main/java/kafka/my/KafkaProducer.java
+++ storm-kafka/src/main/java/kafka/my/KafkaProducer.java
@@ -12,13 +12,14 @@ public class KafkaProducer {
     public static void main(String[] args) {
         String topicName = "test";
         Properties props = new Properties();
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
         props.put("metadata.broker.list", "mini1:9092,mini1:9093,mini1:9094");
         props.put("request.required.acks", "1");
 
         Producer producer = new Producer(new ProducerConfig(props));
 
         for (int messageNo = 1;messageNo<10000;messageNo++){
-            producer.send(new KeyedMessage(topicName,messageNo+"","appid:"+ UUID.randomUUID()+".sanfdre"));
+            producer.send(new KeyedMessage<String,String>(topicName,messageNo+"","appid:"+ UUID.randomUUID().toString()+".sanfdre"));
         }
     }
 
